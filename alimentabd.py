c464218083f6f05c4656c7b95a539de7234f19d3
Guilherme Monteiro <guilhermeromonteiro@gmail.com>
2020-12-13 17:08:09 -0300

    mudancas

--- alimentabd.py
+++ alimentabd.py
@@ -1,11 +1,11 @@
-   def handle(self, *args, **kwargs):
-        path = kwargs['path']
-        with open(path, 'rt') as f:
-            reader = csv.reader(f, dialect='excel')
-            for row in reader:
-                disciplina = Disciplina.objects.create(
-                        sigla = row[0],
-                        cred_aula = row[1],
-                        cred_trabalho = row[2],
-                        ch_total = row[3],
-                        )
+def handle(self, *args, **kwargs):
+    path = kwargs['path']
+    with open(path, 'rt') as f:
+        reader = csv.reader(f, dialect='excel')
+        for row in reader:
+            disciplina = Disciplina.objects.create(
+                sigla=row[0],
+                cred_aula=row[1],
+                cred_trabalho=row[2],
+                ch_total=row[3],
+            )
--- main/models.py
+++ main/models.py
@@ -8,9 +8,6 @@ import csv
 path = r"/Users/luiferreira/Desktop/Sistemas de Informacao/USPavalia/uspavalia/base_dados.csv"
 
 
-
-
-
 # Create your models here.
 class Disciplina(models.Model):
     nome = models.CharField(max_length=100)
@@ -18,20 +15,20 @@ class Disciplina(models.Model):
     cred_aula = models.IntegerField()
     cred_trabalho = models.IntegerField()
     ch_total = models.IntegerField()
-    descricao = models.TextField(max_length=5000) #programacao
+    descricao = models.TextField(max_length=5000)  # programacao
     programa_resumido = models.TextField(max_length=5000)
     bibliografia = models.TextField(max_length=10000)
-    
+
     def __str__(self):
         return self.nome
-
 
 
 class Comentario(models.Model):
     disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     texto = models.TextField(max_length=1000)
-    downVote = models.ManyToManyField(User, blank=True, related_name="downvote")
+    downVote = models.ManyToManyField(
+        User, blank=True, related_name="downvote")
     upVote = models.ManyToManyField(User, blank=True, related_name="upvote")
 
     def __str__(self):
